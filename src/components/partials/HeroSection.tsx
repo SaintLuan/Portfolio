a555574122e1b6043dd45362ed1f57abb480fc27
Luan Santos <50582067+SaintLuan@users.noreply.github.com>
2022-11-26 23:40:14 -0300

    Ajustes (quebra git)

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-<<<<<<< HEAD
-import { FiDribbble, FiFacebook, FiGithub, FiInstagram, FiTwitter } from 'react-icons/fi';
-=======
 import { FiGithub, FiInstagram, FiLinkedin } from 'react-icons/fi';
->>>>>>> 89cd6d1 (first commit)
+
+
 import Button from '@/components/form/Button';
 import HeroBackground from './HeroBackground';
 import Typed from 'react-typed';
@@ -30,33 +28,12 @@ const HeroSection = () => {
           />
         </div>
         <div className="mt-4  flex gap-5">
-<<<<<<< HEAD
-          <Link href="https://www.instragram.com">
-=======
           <Link href="https://www.instagram.com/luan_santos.dev/">
->>>>>>> 89cd6d1 (first commit)
             <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
               <FiInstagram size={25} />
             </a>
           </Link>
-<<<<<<< HEAD
-          <Link href="https://www.twitter.com">
-            <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
-              <FiTwitter size={25} />
-            </a>
-          </Link>
-          <Link href="https://www.facebook.com">
-            <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
-              <FiFacebook size={25} />
-            </a>
-          </Link>
-          <Link href="https://www.dribble.com">
-            <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
-              <FiDribbble size={25} />
-            </a>
-          </Link>
-          <Link href="https://www.github.com">
-=======
+          
 
           <Link href="https://www.linkedin.com/in/luan-santos-864693155">
             <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
@@ -65,11 +42,12 @@ const HeroSection = () => {
           </Link>
           
           <Link href="https://github.com/SaintLuan">
->>>>>>> 89cd6d1 (first commit)
+
             <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
               <FiGithub size={25} />
             </a>
           </Link>
+
         </div>
         <Button className="mt-5 px-8">Get in touch</Button>
       </div>
--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -18,10 +18,6 @@ const ThemeContext = React.createContext<ThemeContextType | undefined>(undefined
 
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const [theme, setTheme] = React.useState<Theme>(Theme.DARK);
-<<<<<<< HEAD
-=======
-  
->>>>>>> 89cd6d1 (first commit)
 
   const toggleTheme = () => {
     if (theme === Theme.LIGHT) {
@@ -37,10 +33,7 @@ const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
 
   useEffect(() => {
     const localTheme = localStorage.getItem('theme');
-<<<<<<< HEAD
-=======
     document.documentElement.classList.add(Theme.DARK);
->>>>>>> 89cd6d1 (first commit)
     if (localTheme) {
       setTheme(localTheme as Theme);
       if (localTheme === Theme.DARK) {
