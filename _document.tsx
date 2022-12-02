2dade7bddd0dc2ca89f41aa4e006cbc4edcd99fd
Luan Santos <50582067+SaintLuan@users.noreply.github.com>
2022-12-02 18:48:03 -0300

    [FIX] Build Header

--- _document.tsx
+++ _document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <Link rel="preconnect" href="https://fonts.googleapis.com" />
-          <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <Link rel="preconnect" href="https://fonts.gstatic.com" />
           <Link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;0,900;1,900&display=swap" rel="stylesheet" />
         </Head>
         <body>
